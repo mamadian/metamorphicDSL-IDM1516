c7dd68281d0469778bea27fbb122f9a516d878ed
Mathieu Acher <mathieu.acher@irisa.fr>
2015-10-19 13:31:27 +0200

    CSS

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 An exercise on domain-specific languages that have different shapes (internal, external) 
 
-Contributions by pull request 
-
+Contributions by pull request
---
+++ dsl.js
@@ -0,0 +1,14 @@
+/* HTML Style */
+document.documentElement.style.background = "#666";
+document.documentElement.style.padding = "1em";
+
+/* Body Style */
+document.body.style.border = "5px dashed #eee";
+document.body.style.color = "#fff";
+document.body.style.font = "3em/1.5 sans-serif";
+document.body.style.padding = "1em";
+document.body.style.width = "30em";
+document.body.style.margin = "0 auto";
+
+/* Body Content */
+document.body.innerHTML = "HelloWorld";
